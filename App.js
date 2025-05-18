c9cf07b2d50191eb317cd6a990c89da83e0db684
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-05-18 15:09:17 -0600

    íconos app

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import { View, StyleSheet } from "react-native";
-import PantallaDeCarga from "./components/PantallaDeCarga";
+import { StatusBar, } from "expo-status-bar";
+import { View, Text, StyleSheet } from "react-native";
 import Constant from "expo-constants";
 
 export default function App() {
-
   return (
-      <View style={styles.container}>
-        <StatusBar style="dark" />
-        <PantallaDeCarga />
-      </View>
+    <View style={styles.container}>
+      <StatusBar style="dark" />
+        <Text>Contenido</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Constant.statusBarHeight,
+    //paddingTop: Constant.statusBarHeight,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
