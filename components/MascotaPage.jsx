3f560b4d35cecd1477d709f603a6fc8c19269644
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-09-25 19:10:07 -0600

    Implemented buttons to change between pets

--- components/MascotaPage.jsx
+++ components/MascotaPage.jsx
@@ -1,42 +1,92 @@
 import { View, Text, StyleSheet, Image, Pressable } from "react-native";
 import { theme } from "../styles/theme";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { EditIcon, AddIcon } from "./Icons";
+import { EditIcon, AddIcon, TriangleIcon } from "./Icons";
 import { Link } from "expo-router";
+import useMascotas from "../hooks/useMascotas";
+import { useState } from "react";
+import { calcularEdad } from "../utils/dates";
 
 function MascotaPage() {
+  // const { isPending, isError, data, error } = useMascotas();
+  const mascotas = [
+    {
+      nombre: "Diego",
+      especie: "Beagle",
+      fechaDeNacimiento: "23/09/2018",
+      avatar: "https://cdn-icons-png.flaticon.com/512/194/194279.png",
+    },
+    {
+      nombre: "Candy",
+      especie: "Gato persa",
+      fechaDeNacimiento: "26/07/2017",
+      avatar:
+        "https://icons.iconarchive.com/icons/iconsmind/outline/512/Cat-icon.png",
+    },
+  ];
+  const [index, setIndex] = useState(0);
+
+  const handleNext = () => {
+    if (index === mascotas.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex((prev) => prev + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (index === 0) {
+      setIndex(mascotas.length - 1);
+    } else {
+      setIndex((prev) => prev - 1);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.screen}>
-      <View style={{ position: "relative" }}>
-        <Image
-          style={styles.avatar}
-          source={{
-            uri: "https://cdn-icons-png.flaticon.com/512/194/194279.png",
-          }}
-        />
-        <Pressable style={styles.editButton}>
-          <EditIcon size={40} color="#000" />
+      <Pressable style={styles.prevButton} onPress={handlePrevious}>
+        <TriangleIcon style={{ transform: "rotate(-90deg)" }} />
+      </Pressable>
+      <View
+        style={{ justifyContent: "center", alignItems: "center", width: "55%" }}
+      >
+        <View style={{ position: "relative" }}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: mascotas[index].avatar,
+            }}
+          />
+          <Pressable style={styles.editButton}>
+            <EditIcon size={40} color="#000" />
+          </Pressable>
+        </View>
+        <Text style={styles.petName}>{mascotas[index].nombre}</Text>
+        <View style={styles.info}>
+          <Text style={styles.subtitle}>INFORMACIÓN</Text>
+          <Text style={styles.text}>Especie: {mascotas[index].especie}</Text>
+          <Text style={styles.text}>
+            Edad: {calcularEdad(mascotas[index].fechaDeNacimiento)}
+          </Text>
+        </View>
+        <Pressable style={styles.button}>
+          <Text
+            style={{
+              color: "#000",
+              textAlign: "center",
+              fontSize: 20,
+              fontFamily: "Montserrat_400Regular",
+            }}
+          >
+            Historial
+          </Text>
         </Pressable>
       </View>
-      <Text style={styles.petName}>DIEGO</Text>
-      <View style={styles.info}>
-        <Text style={styles.subtitle}>INFORMACIÓN</Text>
-        <Text style={styles.text}>Especie: Pitbull</Text>
-        <Text style={styles.text}>Edad: 5</Text>
-      </View>
-      <Pressable style={styles.button}>
-        <Text
-          style={{
-            color: "#000",
-            textAlign: "center",
-            fontSize: 20,
-            fontFamily: "Montserrat_400Regular",
-          }}
-        >
-          Historial
-        </Text>
+      <Pressable style={styles.nextButton} onPress={handleNext}>
+        <TriangleIcon style={{ transform: "rotate(90deg)" }} />
       </Pressable>
-      <Link href={"/agregar-mascota"} asChild>
+
+      <Link href={"/(mascotas)/agregar-mascota"} asChild>
         <Pressable style={styles.addButton}>
           <AddIcon size={70} color="#000" />
         </Pressable>
@@ -51,6 +101,7 @@ const styles = StyleSheet.create({
     backgroundColor: theme.backgroundColor,
     justifyContent: "center",
     alignItems: "center",
+    flexDirection: "row",
   },
   avatar: {
     width: 200,
@@ -77,6 +128,8 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 10,
     marginBottom: 20,
+    justifyContent: "center",
+    alignItems: "center",
   },
   editButton: {
     width: 75,
@@ -90,19 +143,26 @@ const styles = StyleSheet.create({
     right: 0,
   },
   button: {
-    width: "30%",
+    width: "60%",
     height: 50,
     backgroundColor: theme.buttonColor,
     alignContent: "center",
     justifyContent: "center",
     borderRadius: 10,
     marginBottom: 20,
+    paddingHorizontal: 10,
   },
   addButton: {
     height: 75,
     position: "absolute",
     right: 20,
     bottom: 45,
+  },
+  nextButton: {
+    marginLeft: 35,
+  },
+  prevButton: {
+    marginRight: 35,
   },
 });
 
